44b918acb178fa64dc2a36bd93ea165637287de4
Putri Meyliya R <117982575+aikoextended@users.noreply.github.com>
2024-10-17 13:43:32 +0700

    relasi.php

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -8,5 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Genre extends Model
 {
     use HasFactory;
+    
     protected $fillable = ['name'];
+
+    /**
+     * Get the anime reviews for the genre.
+     */
+    public function animeReviews()
+    {
+        return $this->hasMany(AnimeReview::class);
+    }
 }
