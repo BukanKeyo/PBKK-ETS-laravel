98db738a181abc6ef6d756bd4487a3846c954423
Putri Meyliya R <117982575+aikoextended@users.noreply.github.com>
2024-10-17 13:44:16 +0700

    relasi.php

--- app/Models/Title.php
+++ app/Models/Title.php
@@ -8,5 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Title extends Model
 {
     use HasFactory;
+
     protected $fillable = ['name'];
+
+    /**
+     * Get the anime reviews for the title.
+     */
+    public function animeReviews()
+    {
+        return $this->hasMany(AnimeReview::class);
+    }
+
+    /**
+     * Get the genre that the title belongs to.
+     */
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
 }
